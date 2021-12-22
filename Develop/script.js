ad9a5b0f89098ddbd1b0137d6078b2d8de1b7fe8
mona <csimonusa@outlook.com>
2021-12-22 18:07:26 -0500

    03-homework-password-generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,89 @@
+//starter code
 var generateBtn = document.querySelector("#generate")
 
 function writePassword() {
+
   var password = generatePassword()
+
   var passwordText = document.querySelector("#password")
 
   passwordText.value = password
-
 }
 
 generateBtn.addEventListener("click", writePassword)
+//starter code end
 
-var specialCharaters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
+var specialCharaters = ["@%+\\/'!#$^?:,)(}{][~-_."]
 
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numericCharacters = ["0123456789"]
 
-var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var lowerCasedCharacters = ["abcdefghijklmnopqrstuvwxyz"]
 
-var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var upperCasedCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 
 var minLength = 8
 
 var maxLength = 128
 
+var combine = ""
+
 function generatePassword() {
 
-  var Length = prompt("Enter the length of your password.")
+  var passwordLength = prompt("Enter the length of your password.", "between 8 and 128 characters")
+  console.log(passwordLength)
 
-if(Length < minLength || Length > maxLength) {
+  var combine = ""
+
+if(passwordLength < minLength || passwordLength > maxLength) {
 
   alert("Please pick a number between 8 and 128.")
+  return "please try again!"
 
 } else {
 
     var hasSpecialCharacters = confirm("Would you like to include special characters?")
+    console.log("hasSpecialCharacters", hasSpecialCharacters)
 
     var hasNumbers = confirm("Would you like to include numbers?")
+    console.log("hasNumbers", hasNumbers)
 
     var hasLowerCase = confirm("Would you like to include lower case letters?")
+    console.log("hasLowerCase", hasLowerCase)
 
     var hasUpperCase = confirm("Would you like to include upper case letters?")
+    console.log("hasUpperCase", hasUpperCase)
 
     if (!hasLowerCase && !hasUpperCase && !hasNumbers && !hasSpecialCharacters) {
 
       alert("Please choose at least one character type.")
+      return "please try again!"
 
-    }   
+    }
+    if (hasSpecialCharacters) {
+      combine += specialCharaters
+    }
+    if (hasNumbers) {
+      combine += numericCharacters
+    }
+    if (hasLowerCase) {
+      combine += lowerCasedCharacters
+    }
+    if (hasUpperCase) {
+      combine += upperCasedCharacters
+    }
+
+    console.log(combine)
+
+    var result = ""
+
+    for (var i = 0; i < passwordLength; i++) {
+      var random = Math.floor(Math.random() * combine.length)
+      
+      result += combine[random]
+    }
+
+    console.log(result)
+
+    return result
   }
 }
